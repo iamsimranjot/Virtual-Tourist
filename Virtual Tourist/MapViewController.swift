ec95bf239b1c476114ceeafc830a38c865b049c1
iamsimranjot <iamsimranjot@gmail.com>
2017-04-12 12:44:24 +0530

    Add: MapViewController Class Added. Delete: ViewController Class Deleted.

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by SimranJot Singh on 12/04/17.
+//  Copyright © 2017 SimranJot Singh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
