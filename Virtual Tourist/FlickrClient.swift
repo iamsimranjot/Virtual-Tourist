6d6a10c3c4d7bf92d2c99ce8cd89170804918af7
iamsimranjot <iamsimranjot@gmail.com>
2017-04-16 12:40:34 +0530

    Add: Flickr Client Cocoa Class Created.

---
+++ Virtual Tourist/FlickrClient.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrClient.swift
+//  Virtual Tourist
+//
+//  Created by SimranJot Singh on 16/04/17.
+//  Copyright © 2017 SimranJot Singh. All rights reserved.
+//
+
+import Foundation
+
+class FlickrClient {
+
+}
